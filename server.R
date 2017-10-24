c024e690695deba72845400867b773da3787938c
Adam Robinson <adamrobinson361@gmail.com>
2017-10-24 17:38:11 +0100

    Seperate data loading into individual script to be reused

--- server.R
+++ server.R
@@ -8,35 +8,7 @@ source('R/functions.R')
 
 #Load in relevant data  ----------------------------------------------------------------------
 
-Data <- read_csv("Data/deploy_sfr_2016.csv",  col_types = cols(.default = "c"))
-
-#Set SUPP and DNS to NA
-Data[Data == 'SUPP'] <- NA
-Data[Data == "DNS"] <- NA
-
-#Set variables to numeric for plotting
-Data[5:41] <- lapply(Data[,5:41], as.numeric)
-Data[47] <- lapply(Data[,47], as.numeric)
-Data[52:63] <- lapply(Data[,52:63], as.numeric)
-
-#Create custom id column
-Data$ID <- paste(Data$URN,' - ', Data$`School Name`, sep = '')
-
-#list of school characteristics and what school phase they relate to
-characteristics_dd <- read_csv("Data/Characteristics.csv", col_types = cols(.default = "c")) 
-
-#list of characteristics and their type of match for comparisons
-
-characteristics_match <- read_csv("Data/characteristics_match.csv", col_types = cols(.default = "c"))
-
-measure_groupings <- list("Staff Headcount" = colnames(Data)[5:12],
-     "Staff FTE" = colnames(Data)[13:21],
-     "Teacher Characteristics" = colnames(Data)[22:26],
-     "Teaching Assistant Characteristics" = colnames(Data)[27:29],
-     "Non-classroom based Support Staff Characteristics" = colnames(Data)[30:31],
-     "Auxiliary Staff Characteristics" = colnames(Data)[32:33],
-     "Staff Pay" = colnames(Data)[34:37],
-     "Teacher Absence" = colnames(Data)[38:41])
+source("R/load_datasets.R")
 
 #define server logic -------------------------------------------------------------------------
 
--- tests/test-matching.R
+++ tests/test-matching.R
@@ -2,30 +2,15 @@ library(testthat)
 
 context("Matching function")
 
-source('R/functions.R')
+source("R/functions.R")
 
 #Load in core data  ------------------------------------------------------------------------
 
-Data <- read_csv("Data/deploy_sfr_2016.csv",  col_types = cols(.default = "c"))
-
-#Set SUPP and DNS to NA
-Data[Data == 'SUPP'] <- NA
-Data[Data == "DNS"] <- NA
-
-#Set variables to numeric for plotting
-Data[5:41] <- lapply(Data[,5:41], as.numeric)
-Data[47] <- lapply(Data[,47], as.numeric)
-Data[52:63] <- lapply(Data[,52:63], as.numeric)
-
-#Create custom id column
-Data$ID <- paste(Data$URN,' - ', Data$`School Name`, sep = '')
-
-# Load in Characteristics type -------------------------------------------------------------
-characteristics_match <- read_csv("Data/characteristics_match.csv", col_types = cols(.default = "c"))
+source("R/load_datasets.R")
 
 # Load in test data ------------------------------------------------------------------------
 
-test_data <- read_csv("tests/data/subset_test_data.csv", col_types = cols(.default = "c")) %>%
+test_data <- read_csv("tests/Data/subset_test_data.csv", col_types = cols(.default = "c")) %>%
   mutate(number_schls = as.numeric(number_schls))
 
 # Run tests --------------------------------------------------------------------------------
