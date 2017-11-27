d00c391c35caf5836678981b7f82245d3c958246
Adam Robinson <adamrobinson361@gmail.com>
2017-11-27 13:36:14 +0000

    Fix travis conditional deployment (#56)
    * Added conditional deployment
    
    * Fixed travis syntax

--- deploy.R
+++ deploy.R
@@ -8,10 +8,13 @@ app_name <- if (Sys.getenv("TRAVIS_BRANCH") == "master"){
 
 # Set account info
 rsconnect::setAccountInfo(
-  name=Sys.getenv("APP_NAME"),
+  name="department-for-education",
   token=Sys.getenv("SHINYAPPS_TOKEN"),
   secret=Sys.getenv("SHINYAPPS_SECRET")
   )
 
+# Print name to console
+print(app_name)
+
 # Deploy
-rsconnect::deployApp()
+rsconnect::deployApp(appName = app_name)
